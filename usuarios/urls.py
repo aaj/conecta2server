1ba4a5ab76389b61defbe32fc85e62656a56303d
wilguevara <wilrodriguezguevara@gmail.com>
2015-09-08 19:05:47 -0600

    asi deben quedar todas las urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, url
 from . import views
 
 urlpatterns = [
-    url(r'^login/$', views.user_login, name='login'),
+    url(r'^login$', views.user_login, name='login'),
     
-    url(r'^auth/$', views.auth, name='auth'),
-    url(r'^auth/(?P<backend>[^/]+)/$', views.social_auth, name='social_auth'),
+    url(r'^auth$', views.auth, name='auth'),
+    url(r'^auth/(?P<backend>[^/]+)$', views.social_auth, name='social_auth'),
     
-    url(r'^perfil/$', views.perfil, name='perfil'),
+    url(r'^perfil$', views.perfil, name='perfil'),
     # url(r'^register/$', views.register, name='register'),
 ]
